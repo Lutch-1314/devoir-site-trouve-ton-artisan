4f4a86d5d26f89612b45ece4e6b9d7ddd6e5e67b
Lucie Aimar <lucie.aimar13@gmail.com>
2026-03-04 23:00:24 +0100

    front package initialized (react, bootstrap, sass, router), basic configuration on App.jsx, main.jsx

---
+++ trouve-ton-artisan-front/src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+
+import './App.css'
+
+import Home from './pages/Home'
+import ArtisansList from './pages/ArtisansList'
+import ArtisanProfile from './pages/ArtisanProfile'
+import BeingBuilt from './pages/BeingBuilt'
+import NotFound from './pages/NotFound'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/artisans" element={<ArtisansList />}></Route>
+        <Route path="/artisans/:id" element={<ArtisanProfile />}></Route>
+        <Route path="/being-built" element={<BeingBuilt />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
