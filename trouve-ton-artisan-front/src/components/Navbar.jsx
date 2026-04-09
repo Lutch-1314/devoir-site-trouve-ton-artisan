ba4035f85313f3311efd382f96656d46d614f920
Lucie Aimar <lucie.aimar13@gmail.com>
2026-04-09 16:22:57 +0200

    mobile-menu under header ok (useEffect)

--- trouve-ton-artisan-front/src/components/Navbar.jsx
+++ trouve-ton-artisan-front/src/components/Navbar.jsx
@@ -25,6 +25,24 @@ const Navbar = () => {
     fetchCategories();
   }, []);
 
+  useEffect(() => {
+  document.body.classList.toggle("no-scroll", menuOpen);
+}, [menuOpen]);
+
+useEffect(() => {
+  const header = document.querySelector(".header");
+
+  const updateHeight = () => {
+    const height = header.offsetHeight;
+    document.documentElement.style.setProperty("--header-height", `${height}px`);
+  };
+
+  updateHeight();
+  window.addEventListener("resize", updateHeight);
+
+  return () => window.removeEventListener("resize", updateHeight);
+}, []);
+
   return (
     <nav className="custom-navbar">
 
