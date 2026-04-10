69da27f5d07606891f71bd92435dfd3d0ca8930e
Lucie Aimar <lucie.aimar13@gmail.com>
2026-04-10 16:20:31 +0200

    fetch all artisans

--- trouve-ton-artisan-front/src/pages/ArtisansList.jsx
+++ trouve-ton-artisan-front/src/pages/ArtisansList.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import useCategories from "../hooks/useCategories";
 
@@ -10,12 +11,55 @@ const ArtisansList = () => {
   const currentCategory = categories.find(
     (cat) => String(cat.id_categorie || cat.id) === categorieId
   );
+const [artisans, setArtisans] = useState([]);
+
+useEffect(() => {
+  const fetchArtisans = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/artisans");
+      const data = await response.json();
+      setArtisans(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  fetchArtisans();
+}, []);
 
   return (
     <div>
       <h2>
         {currentCategory ? currentCategory.nom : "Artisans"}
       </h2>
+
+      <div className="row">
+  {artisans.map((artisan) => (
+    <div className="col-md-4 mb-4" key={artisan.id}>
+      <div className="artisan-card p-3 h-100 d-flex flex-column justify-content-between">
+
+        <div>
+          <h5 className="artisan-name">{artisan.nom}</h5>
+
+          <p className="artisan-note">⭐ {artisan.note}/5</p>
+
+          <p className="artisan-specialite">
+            {artisan.specialite}
+          </p>
+
+          <p className="artisan-ville text-muted">
+            📍 {artisan.ville}
+          </p>
+        </div>
+
+        <button className="btn btn-link mt-3 text-start p-0">
+          Voir la fiche complète →
+        </button>
+
+      </div>
+    </div>
+  ))}
+</div>
     </div>
   );
 };
