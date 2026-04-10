34fef5be5b19995703d89cf728098e1597a0d818
Lucie Aimar <lucie.aimar13@gmail.com>
2026-04-10 15:58:09 +0200

    ArtisansList page : dynamic h2 (categorie)

--- trouve-ton-artisan-front/src/pages/ArtisansList.jsx
+++ trouve-ton-artisan-front/src/pages/ArtisansList.jsx
@@ -1,6 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+import useCategories from "../hooks/useCategories";
+
 const ArtisansList = () => {
+  const categories = useCategories();
+  const [searchParams] = useSearchParams();
+
+  const categorieId = searchParams.get("categorie");
+
+  const currentCategory = categories.find(
+    (cat) => String(cat.id_categorie || cat.id) === categorieId
+  );
+
   return (
-    <h2>LISTE ARTISANS</h2>
+    <div>
+      <h2>
+        {currentCategory ? currentCategory.nom : "Artisans"}
+      </h2>
+    </div>
   );
 };
 
