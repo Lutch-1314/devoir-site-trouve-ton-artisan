0ae56fb7f718ac714e93e157fe436315577a47e6
Lucie Aimar <lucie.aimar13@gmail.com>
2026-04-22 15:50:35 +0200

    404 page design ok

--- trouve-ton-artisan-front/src/pages/NotFound.jsx
+++ trouve-ton-artisan-front/src/pages/NotFound.jsx
@@ -1,7 +1,9 @@
 const NotFound = () => {
   return (
-    <div>
-      <h1>NotFound</h1>
+    <div className="section default-container not-found">
+      <h1>ERREUR 404</h1>
+      <p>Désolé, mais la page que vous recherchez n'existe pas...</p>
+      <img src="/404-image.png" alt="Erreur 404" />
     </div>
   );
 };
