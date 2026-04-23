05038aa134b212d878363b0b3e0a0b9907321d3f
Lucie Aimar <lucie.aimar13@gmail.com>
2026-04-23 15:36:30 +0200

    search on the names of artisans ok

--- trouve-ton-artisan-front/src/components/SearchBar.jsx
+++ trouve-ton-artisan-front/src/components/SearchBar.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import SearchIcon from "./SearchIcon";
 import "../styles/_searchBar.scss";
 
 const SearchBar = () => {
+
+  const [artisans, setArtisans] = useState([]);
+    const [query, setQuery] = useState("");
+
+useEffect(() => {
+  fetch("http://localhost:3000/api/artisans/noms")
+    .then(res => res.json())
+    .then(data => setArtisans(data));
+}, []);
+
+
+
+  const filteredArtisans = artisans.filter((artisan) =>
+    artisan.toLowerCase().startsWith(query.toLowerCase())
+  );
+
   return (
     <div className="search-bar">
       <div className="search-input-wrapper">
@@ -10,13 +28,27 @@ const SearchBar = () => {
           type="text"
           placeholder="Rechercher"
           className="search-input"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
+
+        
 
         <div className="search-icon-container">
           <SearchIcon />
         </div>
 
       </div>
+
+      {/* résultats */}
+        {query && (
+          <ul className="search-results">
+            {filteredArtisans.map((name, index) => (
+              <li key={index}>{name}</li>
+            ))}
+          </ul>
+        )}
+
     </div>
   );
 };
