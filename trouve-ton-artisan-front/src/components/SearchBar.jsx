8ec38c0fcd4328933d70fa961ffc6c68662be2aa
Lucie Aimar <lucie.aimar13@gmail.com>
2026-04-27 10:39:36 +0200

    in desktop mode: search closed when clicking outside + press ESC; component reorganized

--- trouve-ton-artisan-front/src/components/SearchBar.jsx
+++ trouve-ton-artisan-front/src/components/SearchBar.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import SearchIcon from "./SearchIcon";
 import "../styles/_searchBar.scss";
@@ -9,20 +8,52 @@ const SearchBar = ({ onSelect }) => {
   const [artisans, setArtisans] = useState([]);
   const [query, setQuery] = useState("");
 
+  const wrapperRef = useRef(null);
+
+  const navigate = useNavigate();
+
   useEffect(() => {
     fetch("http://localhost:3000/api/artisans/noms")
       .then(res => res.json())
       .then(data => setArtisans(data));
   }, []);
 
+  useEffect(() => {
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setQuery("");
+      onSelect?.(); // ferme si besoin
+    }
+  };
+
+  document.addEventListener("mousedown", handleClickOutside);
+
+  return () => {
+    document.removeEventListener("mousedown", handleClickOutside);
+  };
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        setQuery("");
+        onSelect?.();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onSelect]);
+
   const filteredArtisans = artisans.filter((artisan) =>
     artisan.nom.toLowerCase().startsWith(query.toLowerCase())
   );
 
-  const navigate = useNavigate();
-
   return (
-    <div className="search-bar">
+    <div className="search-bar" ref={wrapperRef}>
       <div className="search-input-wrapper">
 
         <input
@@ -48,7 +79,7 @@ const SearchBar = ({ onSelect }) => {
                 onClick={() => {
                   navigate(`/artisans/${artisan.id}`);
                   setQuery("");
-                  onSelect(); // ferme la barre
+                  onSelect?.(); // ferme la barre
                 }}
               >
                 {artisan.nom}
