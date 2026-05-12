f8f490fce333ade5648fb18a784b999cefd6c9eb
Lucie Aimar <lucie.aimar13@gmail.com>
2026-05-12 21:54:38 +0200

    W3C check (html + css) ok; a css element moved to _layout.scss; .env files renamed & names updated in gitignore; index.js : FRONTEND_URL used

--- trouve-ton-artisan-api/index.js
+++ trouve-ton-artisan-api/index.js
@@ -1,47 +1,46 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowed = [
+        process.env.FRONTEND_URL,
+        "http://localhost:5173",
+      ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // autorise localhost
-    if (!origin) return callback(null, true);
+      if (!origin || allowed.includes(origin)) {
+        return callback(null, true);
+      }
 
-    // autorise Vercel (tous les déploiements)
-    if (origin.endsWith(".vercel.app")) {
-      return callback(null, true);
-    }
-
-    // ou ton domaine exact prod si tu en as un
-    return callback(new Error("Not allowed by CORS"));
-  }
-}));
+      return callback(null, false);
+    },
+  }),
+);
 
 app.use(express.json());
 
-const sequelize = require('./db');
+const sequelize = require("./db");
 
-sequelize.authenticate()
-  .then(() => console.log('Connexion MySQL OK'))
-  .catch(err => console.error('Erreur de connexion :', err));
+sequelize
+  .authenticate()
+  .then(() => console.log("Connexion MySQL OK"))
+  .catch((err) => console.error("Erreur de connexion :", err));
 
-app.use('/images', express.static('public/images'));
+app.use("/images", express.static("public/images"));
 
-const artisanRoutes = require('./routes/artisanRoutes');
-app.use('/api/artisans', artisanRoutes);
+const artisanRoutes = require("./routes/artisanRoutes");
+app.use("/api/artisans", artisanRoutes);
 
-const categorieRoutes = require('./routes/categorieRoutes');
-app.use('/api/categories', categorieRoutes);
+const categorieRoutes = require("./routes/categorieRoutes");
+app.use("/api/categories", categorieRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API fonctionne !');
+app.get("/", (req, res) => {
+  res.send("API fonctionne !");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
