66c4df407e5444906d5dc2e1511de87ce5ef4439
Lucie Aimar <lucie.aimar13@gmail.com>
2026-05-12 22:11:51 +0200

    Secure API (rate limit, helmet, CORS whitelist)

--- trouve-ton-artisan-api/index.js
+++ trouve-ton-artisan-api/index.js
@@ -2,16 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
 
 app.use(
   cors({
     origin: (origin, callback) => {
-      const allowed = [
-        process.env.FRONTEND_URL,
-        "http://localhost:5173",
-      ];
+      const allowed = [process.env.FRONTEND_URL, "http://localhost:5173"];
 
       if (!origin || allowed.includes(origin)) {
         return callback(null, true);
@@ -32,6 +34,16 @@ sequelize
   .catch((err) => console.error("Erreur de connexion :", err));
 
 app.use("/images", express.static("public/images"));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 100,
+  message: {
+    error: "Trop de requêtes, réessaie plus tard.",
+  },
+});
+
+app.use("/api", limiter);
 
 const artisanRoutes = require("./routes/artisanRoutes");
 app.use("/api/artisans", artisanRoutes);
